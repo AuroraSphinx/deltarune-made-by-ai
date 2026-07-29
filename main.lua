15a24a28f868489a1167160567a2a3e1aeb4f529
AuroraSphinx <ribeirosamara200@gmail.com>
2026-07-29 17:17:35 -0300

    simplify main.lua to rectangle test

--- main.lua
+++ main.lua
@@ -1,26 +1,14 @@
-local Game = require("src.game")
-
 function love.load()
-    love.graphics.setDefaultFilter("nearest", "nearest")
-    love.keyboard.setKeyRepeat(false)
-    Game:load()
+    player = {x = 100, y = 100, speed = 200}
 end
 
 function love.update(dt)
-    Game:update(math.min(dt, 1 / 20))
+    if love.keyboard.isDown("w") then player.y = player.y - player.speed * dt end
+    if love.keyboard.isDown("s") then player.y = player.y + player.speed * dt end
+    if love.keyboard.isDown("a") then player.x = player.x - player.speed * dt end
+    if love.keyboard.isDown("d") then player.x = player.x + player.speed * dt end
 end
 
 function love.draw()
-    Game:draw()
-end
-
-function love.keypressed(key, scancode, isrepeat)
-    if isrepeat then
-        return
-    end
-    Game:keypressed(key, scancode)
-end
-
-function love.resize(width, height)
-    Game:resize(width, height)
+    love.graphics.rectangle("fill", player.x, player.y, 32, 32)
 end
